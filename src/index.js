4f14c8a13c9c66306238b31120ca19960b02d78c
Paramesh-sabri <parameshsabri@gmail.com>
2024-08-22 16:15:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"; // Optional: if you want to add global styles
+import EmployeeForm from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <EmployeeForm />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
